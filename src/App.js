5dc14259a58825d82f056e3f7481419d06a16671
Brandon Pulera <pulerabj11@gmail.com>
2022-04-13 17:01:02 -0500

    Basic nav bar set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p id="headerName">Brandon Pulera</p>
+        <nav>
+          <ul id="nav__links">
+            <li> <a href="#">Projects</a></li>
+            <li> <a href="#">Resume</a></li>
+            <li> <a href="#">About</a></li>
+          </ul>
+        </nav>
+        <a id="contactButton" href="#"><button>Contact</button></a>
       </header>
+
+      <body>
+        
+      </body>
     </div>
   );
 }
